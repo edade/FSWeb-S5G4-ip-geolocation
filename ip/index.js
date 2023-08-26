73b816bb9e0e0ad49064a48204b1e21b424360ec
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-08-26 19:15:21 +0300

    adımlar tamamlandı

--- ip/index.js
+++ ip/index.js
@@ -29,16 +29,19 @@ async function ipAdresimiAl() {
 	NOT: Bilgisayarın IP adresini öğrenmek için: https://apis.ergineer.com/ipadresim 
 	ADIM 5'e gelene kadar fonksiyonunuzu test etmek için ip nizi URL'ye manuel olarak ekleyebilirsiniz.
 */
-benimIP = "95.8.193.135";
-const url = "https://apis.ergineer.com/ipgeoapi/" + benimIP;
-axios
-  .get(url)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+ipAdresimiAl().then(() => {
+  const url = "https://apis.ergineer.com/ipgeoapi/" + benimIP;
+  axios
+    .get(url)
+    .then((response) => {
+      const component = Fonks(response.data);
+      document.querySelector(".cards").append(component);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 /*
 	ADIM 2: Geri döndürülen verileri inceleyin, bu sizin ip bilgileriniz! Bileşen fonksiyonunuzu geliştirmek içindeki bu veri yapısını
 	iyice anlamanız gerekmektedir.
@@ -61,6 +64,42 @@ axios
 	</div>
     </div>
 */
+function Fonks(data) {
+  const div = document.createElement("div");
+  div.classList.add("card");
+  const img = document.createElement("img");
+  img.src = data.ülkebayrağı;
+  const div2 = document.createElement("div");
+  div2.setAttribute("class", "card-info");
+
+  const h3 = document.createElement("h3");
+  h3.classList.add("ip");
+  h3.textContent = data.sorgu;
+
+  const p = document.createElement("p");
+  p.classList.add("ulke");
+  p.textContent = `${data.ülke} (${data.ülkeKodu})`;
+
+  const p2 = document.createElement("p");
+  p2.textContent = `Enlem: ${data.enlem} Boylam: ${data.boylam}`;
+
+  const p3 = document.createElement("p");
+  p3.textContent = `Şehir: ${data.şehir}`;
+
+  const p4 = document.createElement("p");
+  p4.textContent = `Saat dilimi: ${data.saatdilimi}`;
+
+  const p5 = document.createElement("p");
+  p5.textContent = `Para birimi: ${data.parabirimi}`;
+
+  const isp = document.createElement("p");
+  isp.textContent = `ISP: ${data.isp}`;
+
+  div.append(img, div2);
+  div2.append(h3, p, p2, p3, p4, p5, isp);
+
+  return div;
+}
 
 /*
 	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve 
